6fa7edf4901a93d8250ec11b6c6f3469be30b101
Peter Ekler <peter.ekler@aut.bme.hu>
2015-08-18 14:27:32 +0200

    method name print by checking the stacktrace

--- Chapter5/ActivityLifeCycle/app/src/main/java/hu/bme/aut/androidforprof/activitylifecycle/MainActivity.java
+++ Chapter5/ActivityLifeCycle/app/src/main/java/hu/bme/aut/androidforprof/activitylifecycle/MainActivity.java
@@ -2,37 +2,54 @@ package hu.bme.aut.androidforprof.activitylifecycle;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
 
 public class MainActivity extends ActionBarActivity {
 
+    public static final String ACTIITY_LIFECYCLE = "ACTIITY_LIFECYCLE";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        logMethodName();
         setContentView(R.layout.activity_main);
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+    protected void onStart() {
+        super.onStart();
+        logMethodName();
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+    protected void onResume() {
+        super.onResume();
+        logMethodName();
+    }
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
+    @Override
+    protected void onPause() {
+        super.onPause();
+        logMethodName();
+    }
 
-        return super.onOptionsItemSelected(item);
+    @Override
+    protected void onStop() {
+        super.onStop();
+        logMethodName();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        logMethodName();
+    }
+
+    public static void logMethodName() {
+        final StackTraceElement[] ste = Thread.currentThread().getStackTrace();
+        Log.d(ACTIITY_LIFECYCLE, ste[3].getMethodName()+" method called.");
     }
 }

aa20ab722a46bcf47771806ecdfc844c04d1b1be
Peter Ekler <peter.ekler@aut.bme.hu>
2015-08-18 18:01:06 +0200

    ActionBarActivity->AppCompatActivity

--- Chapter5/ActivityLifeCycle/app/src/main/java/hu/bme/aut/androidforprof/activitylifecycle/MainActivity.java
+++ Chapter5/ActivityLifeCycle/app/src/main/java/hu/bme/aut/androidforprof/activitylifecycle/MainActivity.java
@@ -2,18 +2,20 @@ package hu.bme.aut.androidforprof.activitylifecycle;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends AppCompatActivity {
 
     public static final String ACTIITY_LIFECYCLE = "ACTIITY_LIFECYCLE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.i(ACTIITY_LIFECYCLE, "onCreate() called.");
         logMethodName();
         setContentView(R.layout.activity_main);
     }
@@ -21,30 +23,35 @@ public class MainActivity extends ActionBarActivity {
     @Override
     protected void onStart() {
         super.onStart();
+        Log.i(ACTIITY_LIFECYCLE, "onCreate() called.");
         logMethodName();
     }
 
     @Override
     protected void onResume() {
         super.onResume();
+        Log.i(ACTIITY_LIFECYCLE, "onCreate() called.");
         logMethodName();
     }
 
     @Override
     protected void onPause() {
         super.onPause();
+        Log.i(ACTIITY_LIFECYCLE, "onCreate() called.");
         logMethodName();
     }
 
     @Override
     protected void onStop() {
         super.onStop();
+        Log.i(ACTIITY_LIFECYCLE, "onCreate() called.");
         logMethodName();
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
+        Log.i(ACTIITY_LIFECYCLE, "onCreate() called.");
         logMethodName();
     }
 

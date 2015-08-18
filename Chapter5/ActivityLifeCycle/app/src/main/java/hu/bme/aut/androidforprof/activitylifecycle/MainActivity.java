9c394ec3447b3f5cf96ce7cea96c1f61eb1f8226
Peter Ekler <peter.ekler@aut.bme.hu>
2015-08-18 18:02:57 +0200

    typo fix

--- Chapter5/ActivityLifeCycle/app/src/main/java/hu/bme/aut/androidforprof/activitylifecycle/MainActivity.java
+++ Chapter5/ActivityLifeCycle/app/src/main/java/hu/bme/aut/androidforprof/activitylifecycle/MainActivity.java
@@ -1,21 +1,18 @@
 package hu.bme.aut.androidforprof.activitylifecycle;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
 
 
 public class MainActivity extends AppCompatActivity {
 
-    public static final String ACTIITY_LIFECYCLE = "ACTIITY_LIFECYCLE";
+    public static final String ACTIVITY_LIFECYCLE = "ACTIVITY_LIFECYCLE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Log.i(ACTIITY_LIFECYCLE, "onCreate() called.");
+        Log.i(ACTIVITY_LIFECYCLE, "onCreate() called.");
         logMethodName();
         setContentView(R.layout.activity_main);
     }
@@ -23,40 +20,40 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onStart() {
         super.onStart();
-        Log.i(ACTIITY_LIFECYCLE, "onCreate() called.");
+        Log.i(ACTIVITY_LIFECYCLE, "onCreate() called.");
         logMethodName();
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        Log.i(ACTIITY_LIFECYCLE, "onCreate() called.");
+        Log.i(ACTIVITY_LIFECYCLE, "onCreate() called.");
         logMethodName();
     }
 
     @Override
     protected void onPause() {
         super.onPause();
-        Log.i(ACTIITY_LIFECYCLE, "onCreate() called.");
+        Log.i(ACTIVITY_LIFECYCLE, "onCreate() called.");
         logMethodName();
     }
 
     @Override
     protected void onStop() {
         super.onStop();
-        Log.i(ACTIITY_LIFECYCLE, "onCreate() called.");
+        Log.i(ACTIVITY_LIFECYCLE, "onCreate() called.");
         logMethodName();
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        Log.i(ACTIITY_LIFECYCLE, "onCreate() called.");
+        Log.i(ACTIVITY_LIFECYCLE, "onCreate() called.");
         logMethodName();
     }
 
     public static void logMethodName() {
         final StackTraceElement[] ste = Thread.currentThread().getStackTrace();
-        Log.d(ACTIITY_LIFECYCLE, ste[3].getMethodName()+" method called.");
+        Log.d(ACTIVITY_LIFECYCLE, ste[3].getMethodName()+" method called.");
     }
 }

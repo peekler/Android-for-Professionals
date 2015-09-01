7e629681c2488eea03f9fedca90f7581814c271c
Peter Ekler <peter.ekler@aut.bme.hu>
2015-09-01 08:21:45 +0200

    log messages fixed in MainActivity

--- Chapter5/ActivityLifeCycle/app/src/main/java/hu/bme/aut/androidforprof/activitylifecycle/MainActivity.java
+++ Chapter5/ActivityLifeCycle/app/src/main/java/hu/bme/aut/androidforprof/activitylifecycle/MainActivity.java
@@ -12,6 +12,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // Activity is being created.
         Log.i(ACTIVITY_LIFECYCLE, "onCreate() called.");
         logMethodName();
         setContentView(R.layout.activity_main);
@@ -20,40 +21,47 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onStart() {
         super.onStart();
-        Log.i(ACTIVITY_LIFECYCLE, "onCreate() called.");
+        // Activity becomes visible.
+        Log.i(ACTIVITY_LIFECYCLE, "onStart() called.");
         logMethodName();
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        Log.i(ACTIVITY_LIFECYCLE, "onCreate() called.");
+        // Activity is visible and has focus.
+        Log.i(ACTIVITY_LIFECYCLE, "onResume() called.");
         logMethodName();
     }
 
     @Override
     protected void onPause() {
         super.onPause();
-        Log.i(ACTIVITY_LIFECYCLE, "onCreate() called.");
+        // Another Activity gets focus,
+        // this Activity will go into pause state.
+        Log.i(ACTIVITY_LIFECYCLE, "onPause() called.");
         logMethodName();
     }
 
     @Override
     protected void onStop() {
         super.onStop();
-        Log.i(ACTIVITY_LIFECYCLE, "onCreate() called.");
+        // The Activity is not visible,
+        // the Activity is in stopped state.
+        Log.i(ACTIVITY_LIFECYCLE, "onStop() called.");
         logMethodName();
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        Log.i(ACTIVITY_LIFECYCLE, "onCreate() called.");
+        // The Activity will destroy.
+        Log.i(ACTIVITY_LIFECYCLE, "onDestroy() called.");
         logMethodName();
     }
 
     public static void logMethodName() {
         final StackTraceElement[] ste = Thread.currentThread().getStackTrace();
-        Log.d(ACTIVITY_LIFECYCLE, ste[3].getMethodName()+" method called.");
+        Log.i(ACTIVITY_LIFECYCLE, ste[3].getMethodName()+" method called.");
     }
 }
